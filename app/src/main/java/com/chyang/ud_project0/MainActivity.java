69814f386b94f053763783c55a6781b19f1c6a97
caihui2 <chyangbest@icloud.com>
2016-09-26 15:15:49 +0800

    c

--- app/src/main/java/com/chyang/ud_project0/MainActivity.java
+++ app/src/main/java/com/chyang/ud_project0/MainActivity.java
@@ -2,12 +2,32 @@ package com.chyang.ud_project0;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        if(view instanceof Button) {
+            Button bt = (Button) view;
+            showToast( "This button will launch my " + bt.getText().toString().toLowerCase() + " apps! ");
+        }
+    }
+
+    private void showToast(String text) {
+        if(mToast == null) {
+            mToast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
+        }
+        mToast.setText(text);
+        mToast.show();
     }
 }
